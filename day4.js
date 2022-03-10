793afa89bd104984413c6f228b23271ac84f59c0
ratamon-tech <ratamon91@gmail.com>
2022-03-10 09:47:59 +0900

    day4.js作成、標準出力のメモ記載

---
+++ day4.js
@@ -0,0 +1,9 @@
+// 標準入力のメモ
+
+/*
+参考
+https://qiita.com/saba_can00/items/02ff28a16a0d312a5259
+https://lamarr.dev/ja/javascript/2020/04/06/01.html
+*/
+
+console.log("JavaScript(Node.js)は標準入力がちょっと工夫いる")
